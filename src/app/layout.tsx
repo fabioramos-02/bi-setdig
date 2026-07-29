096c158f5b24486aa32249c10961031f00313645
Fabio Willian Lima Ramos <framos@segov.ms.gov.br>
2026-07-29 14:44:15 -0400

    feat: initialize root layout with Open Sans font, theme provider, and analytics integration

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Open_Sans } from "next/font/google";
+import Script from "next/script";
 import { ThemeProvider } from "next-themes";
 import "./globals.css";
 
@@ -22,6 +23,13 @@ export default function RootLayout({
     <html lang="pt-BR" className={`${openSans.variable} h-full`} suppressHydrationWarning>
       <head>
         <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet" />
+        <Script
+          src="https://plausible.io/js/pa-YFgILOkHZ2-PDKZGoeFpU.js"
+          strategy="afterInteractive"
+        />
+        <Script id="plausible-init" strategy="afterInteractive">
+          {`window.plausible=window.plausible||function(){(plausible.q=plausible.q||[]).push(arguments)},plausible.init=plausible.init||function(i){plausible.o=i||{}};plausible.init()`}
+        </Script>
       </head>
       <body className="min-h-full flex flex-col">
         <ThemeProvider attribute="data-theme" defaultTheme="light" enableSystem>
